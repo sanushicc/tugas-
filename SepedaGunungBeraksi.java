3fcaec13e81326c10010b429fa46ab777979daa1
SanushiCc <71433926+sanushicc@users.noreply.github.com>
2021-03-10 15:56:36 +0700

    Add files via upload

---
+++ SepedaGunungBeraksi.java
@@ -0,0 +1,13 @@
+package com.tugas_inheritance;
+
+class SepedaGunungBeraksi {
+    public static void main(String[] args) {
+
+        Sepedagunung sepedaku = new Sepedagunung();
+        Sepeda sepedame = new Sepeda();
+
+        sepedame.tambahkecepatan(10);
+        sepedame.ubahgir(2);
+        sepedaku.setSadel(20);
+    }
+}
---
+++ Sepedagunung.java
@@ -0,0 +1,13 @@
+package com.tugas_inheritance;
+
+class Sepedagunung {
+
+    int setSadel;
+
+    public void setSadel(int nilaiSadel) {
+        setSadel = nilaiSadel;
+
+        System.out.println("Tinggi Sadel : " + setSadel);
+    }
+
+}
